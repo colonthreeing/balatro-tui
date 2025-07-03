1ccf8e89d0db502fe0ea8f10720f91acf0a8cc79
julie <itsjuliestromberg@gmail.com>
2025-07-02 17:34:34 -0700

    more motds

--- src/motd.rs
+++ src/motd.rs
@@ -1,3 +1,22 @@
+use rand::seq::{IndexedRandom};
+use rand::rng;
+
 pub fn motd() -> String {
-    String::from("Welcome to the Balatro TUI!")
+    let potentials = vec![
+        "First we Ball, then we Atro. We Balatro!",
+        "Someone call Bean!",
+        "Surely you'll pull a baron this time.",
+        "Also try the SealSeal mod!",
+        "You are valid!",
+        "Don't buff ceremonial dagger.",
+        "Praise Zech!",
+        "Only cool people can see this message :3",
+        ":3",
+        "Also try Cloverpit!",
+        "You can add your own MOTD by opening a report at https://github.com/colonthreeing/balatro-tui!",
+        "Welcome to the balatro tui!"
+    ];
+
+    let random = potentials.choose(&mut rng());
+    random.unwrap().to_string()
 }
