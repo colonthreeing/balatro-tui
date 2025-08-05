8b8cef10ad36ffb42553d948c7e0071eaf27f27f
julie <itsjuliestromberg@gmail.com>
2025-08-05 00:30:55 -0700

    Fix installing mods with no folder_name specified

--- src/mods.rs
+++ src/mods.rs
@@ -1,13 +1,13 @@
+use crate::config::get_data_dir;
+use balatro_tui::get_balatro_appdata_dir;
+use git2::Repository;
+use log::{error, info};
+use serde::Deserialize;
+use serde::de::DeserializeOwned;
+use serde_json::Value;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
-use git2::Repository;
-use log::{error, info};
-use serde::de::DeserializeOwned;
-use serde::Deserialize;
-use serde_json::Value;
-use balatro_tui::get_balatro_appdata_dir;
-use crate::config::get_data_dir;
 
 #[derive(Default)]
 pub struct ModList {
@@ -21,23 +21,29 @@ impl ModList {
 
     pub fn get_local_mods() -> Vec<Mod> {
         let mod_path = get_balatro_appdata_dir().join("Mods");
-        
+
         let mut mods = vec![];
         if let Some(dir) = std::fs::read_dir(mod_path.clone()).ok() {
             for entry in dir {
                 let entry = entry.unwrap();
                 let path = entry.path();
 
-                if !path.is_dir() { continue; }
+                if !path.is_dir() {
+                    continue;
+                }
 
                 let mut found_mod_meta = false;
 
                 for file in std::fs::read_dir(&path).unwrap() {
                     let file = file.unwrap();
                     let filepath = file.path();
-                    if !filepath.is_file() { continue; }
+                    if !filepath.is_file() {
+                        continue;
+                    }
                     if let Some(extension) = filepath.extension().and_then(|e| e.to_str()) {
-                        if extension != "json" { continue; }
+                        if extension != "json" {
+                            continue;
+                        }
                         if let Some(mut mod_obj) = Mod::from_file(&file.path()) {
                             if mod_obj.id.is_empty() {
                                 if mod_obj.name == "Steamodded" {
@@ -47,7 +53,8 @@ impl ModList {
                                     mod_obj.version = "1.0.0".to_string();
                                     mod_obj.enabled = Some(mod_obj.get_enabled());
                                     mod_obj.force_enable = true;
-                                    mod_obj.author = vec!["the Steamodded contributors".to_string()];
+                                    mod_obj.author =
+                                        vec!["the Steamodded contributors".to_string()];
 
                                     // get version from version.lua
                                     // version.lua format is literally just
@@ -56,7 +63,15 @@ impl ModList {
                                     let f = File::open(path.join("version.lua")).unwrap();
                                     let reader = BufReader::new(f);
                                     // This feels bad to be doing *but* it works.
-                                    mod_obj.version = reader.lines().nth(0).unwrap().unwrap().split("\"").nth(1).unwrap().to_string();
+                                    mod_obj.version = reader
+                                        .lines()
+                                        .nth(0)
+                                        .unwrap()
+                                        .unwrap()
+                                        .split("\"")
+                                        .nth(1)
+                                        .unwrap()
+                                        .to_string();
 
                                     mods.push(mod_obj);
                                     found_mod_meta = true;
@@ -72,7 +87,11 @@ impl ModList {
 
                 if !found_mod_meta {
                     let name = path.file_name().unwrap().to_str().unwrap().to_string();
-                    if name.to_lowercase().starts_with("lovely") || name.to_lowercase().starts_with("steamodded") { continue; }
+                    if name.to_lowercase().starts_with("lovely")
+                        || name.to_lowercase().starts_with("steamodded")
+                    {
+                        continue;
+                    }
                     let mut mod_obj = Mod::new();
                     mod_obj.folder = path.clone();
                     mod_obj.enabled = Some(mod_obj.get_enabled());
@@ -91,7 +110,7 @@ impl ModList {
 
         let mut mods_dir = get_data_dir();
         mods_dir.extend(
-            ["mods", "mods"] // mods folder inside the repository
+            ["mods", "mods"], // mods folder inside the repository
         );
 
         if let Some(dir) = std::fs::read_dir(mods_dir.clone()).ok() {
@@ -99,7 +118,9 @@ impl ModList {
                 let entry = entry.unwrap();
                 let path = entry.path();
 
-                if !path.is_dir() { continue; }
+                if !path.is_dir() {
+                    continue;
+                }
                 if let Some(mod_obj) = RemoteMod::from_directory(&path) {
                     mods.push(mod_obj);
                 }
@@ -124,7 +145,7 @@ pub struct Mod {
     pub author: Vec<String>,
 
     pub dependencies: Vec<String>,
-    
+
     #[serde(default)]
     pub enabled: Option<bool>,
 
@@ -152,11 +173,17 @@ impl Mod {
         for file in std::fs::read_dir(&path).unwrap() {
             let file = file.unwrap();
             let filepath = file.path();
-            if !filepath.is_file() { continue; }
+            if !filepath.is_file() {
+                continue;
+            }
             if let Some(extension) = filepath.extension().and_then(|e| e.to_str()) {
-                if extension != "json" { continue; }
+                if extension != "json" {
+                    continue;
+                }
                 if let Some(mut mod_obj) = Mod::from_file(&file.path()) {
-                    if mod_obj.id.is_empty() { continue; }
+                    if mod_obj.id.is_empty() {
+                        continue;
+                    }
                     mod_obj.enabled = Some(mod_obj.get_enabled());
                     found_mod = mod_obj;
                 }
@@ -181,7 +208,7 @@ impl Mod {
         for entry in std::fs::read_dir(&self.folder).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
-            
+
             if let Some(name) = path.file_name().and_then(|e| e.to_str()) {
                 if name == ".lovelyignore" {
                     enabled = false;
@@ -203,7 +230,8 @@ impl Mod {
             std::fs::File::create(&self.folder.join(".lovelyignore")).unwrap();
             self.enabled = Some(false);
         } else {
-            std::fs::remove_file(&self.folder.join(".lovelyignore")).expect("Failed to remove .lovelyignore file");
+            std::fs::remove_file(&self.folder.join(".lovelyignore"))
+                .expect("Failed to remove .lovelyignore file");
             self.enabled = Some(true);
         }
     }
@@ -243,7 +271,9 @@ impl RemoteMod {
         for file in std::fs::read_dir(&path).unwrap() {
             let file = file.unwrap();
             let filepath = file.path();
-            if !filepath.is_file() { continue; }
+            if !filepath.is_file() {
+                continue;
+            }
             if let Some(filename) = filepath.file_name().and_then(|e| e.to_str()) {
                 if filename == "meta.json" {
                     let reader = BufReader::new(File::open(&filepath).unwrap());
@@ -256,13 +286,17 @@ impl RemoteMod {
         found_mod.identifier = path.file_name()?.to_str()?.to_string();
 
         Some(Self {
-            title: found_mod.title,
-            version: found_mod.version,
+            title: found_mod.title.clone(),
+            version: found_mod.version.clone(),
             author: found_mod.author,
             categories: found_mod.categories,
             repo: found_mod.repo,
             download_url: found_mod.download_url,
-            folder_name: found_mod.folder_name,
+            folder_name: if found_mod.folder_name != "" {
+                found_mod.folder_name
+            } else {
+                found_mod.title
+            },
             identifier: found_mod.identifier,
         })
     }
